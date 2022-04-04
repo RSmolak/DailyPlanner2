4e36386f4520dca8b87310a77e50059180a44865
rafal.smolak <rafal.smolak@gmail.com>
2022-04-04 20:51:57 +0200

    Add project files.

---
+++ DailyPlanner2/DataModels/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPlanner2.DataModels
+{
+    public class Context : DbContext
+    {
+        DbSet<Task> tasks {get; set;}
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=DailyPlanner;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ DailyPlanner2/DataModels/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPlanner2.DataModels
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }  
+        public int Status { get; set; } 
+        public DateOnly DateOnly { get; set; }
+
+    }
+}
